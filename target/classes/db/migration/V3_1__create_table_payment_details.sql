dba91585e8630df2dab733d56b696a9f1c5552ec
luis.manzanaro@gmail.com <luis.manzanaro@gmail.com>
2022-12-15 15:54:54 +0100

    Fix properties

--- target/classes/db/migration/V3_1__create_table_payment_details.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "order".payment_details
-(
-    order_id int8 PRIMARY KEY,
-    credit_number int8 not null,
-    expiration_month varchar(2) not null,
-    expiration_year varchar(4) not null,
-    cvv varchar(3) not null
-);
